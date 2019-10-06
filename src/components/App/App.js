76dd931ea40c54c0d46f455ba6dd7425685c0aac
Ilya Nee <neeilya@icloud.com>
2019-10-06 18:00:34 +0200

    Apply global theme

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,21 +4,28 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Popup from '../../views/Popup/Popup';
 import Options from '../../views/Options/Options';
+import { theme } from '../../theme';
 
 const App = () => (
-  <Router>
-    <Switch>
-      <Route path="/popup">
-        <Popup />
-      </Route>
-      <Route path="/options">
-        <Options />
-      </Route>
-    </Switch>
-  </Router>
+  <MuiThemeProvider theme={createMuiTheme(theme)}>
+    <CssBaseline>
+      <Router>
+        <Switch>
+          <Route path="/popup">
+            <Popup />
+          </Route>
+          <Route path="/options">
+            <Options />
+          </Route>
+        </Switch>
+      </Router>
+    </CssBaseline>
+  </MuiThemeProvider>
 );
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,27 @@
+export const colors = {
+  accentPrimary: '#4CAF50',
+  accentPrimaryDark: '#43A047',
+  accentPrimaryLight: '#66BB6A',
+  accentPrimaryLightest: '#81C784',
+};
+
+export const palette = {
+  primary: {
+    main: colors.accentPrimary,
+    dark: colors.accentPrimaryDark,
+    light: colors.accentPrimaryLight,
+    lightest: colors.accentPrimaryLightest
+  }
+};
+
+export const typography = {
+  fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+  fontSize: 14,
+  useNextVariants: true
+};
+
+export const theme = {
+  colors,
+  palette,
+  typography
+};
