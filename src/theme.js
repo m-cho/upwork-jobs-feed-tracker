6f429f1146ea5594ac3b1db52fb1382c337689be
Ilya Nee <neeilya@icloud.com>
2019-10-08 20:03:15 +0200

    Update theme styles

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,13 @@ export const colors = {
   accentPrimary: '#4CAF50',
   accentPrimaryDark: '#43A047',
   accentPrimaryLight: '#66BB6A',
-  accentPrimaryLightest: '#81C784'
+  accentPrimaryLightest: '#81C784',
+
+  darkGrey: '#525252',
+  white: '#fff',
+
+  backgroundDark: '#303030',
+  backgroundLight: '#e2e2e2'
 };
 
 export const palette = {
@@ -16,12 +22,34 @@ export const palette = {
 
 export const typography = {
   fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
-  fontSize: 14,
+  fontSize: 13,
   useNextVariants: true
 };
 
-export const theme = {
+export const baseTheme = {
   colors,
   palette,
   typography
 };
+
+export const getTheme = darkMode => ({
+  ...baseTheme,
+  palette: {
+    ...baseTheme.palette,
+    type: darkMode
+      ? 'dark'
+      : 'light',
+    text: {
+      ...baseTheme.text,
+      primary: darkMode
+        ? colors.white
+        : colors.darkGrey
+    },
+    background: {
+      ...baseTheme.palette.background,
+      default: darkMode
+        ? colors.backgroundDark
+        : colors.backgroundLight
+    }
+  }
+});
